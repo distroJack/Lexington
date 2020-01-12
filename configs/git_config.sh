34180a0d96830d97224063a3e4998738aa07d54d
jack <jswim26@gmail.com>
2020-01-11 21:39:08 -0500

    [configs] git config

---
+++ configs/git_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config --global user.name "Jack"
+git config --global user.email "jswim26@gmail.com"
+
+git config --global alias.a "add"
+git config --global alias.b "branch"
+git config --global alais.c "commit"
+git config --global alias.p "push"
+git config --global alias.s "status"
+
+git config --global merge.tool "gedit"
