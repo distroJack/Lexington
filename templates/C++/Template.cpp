a9b6a78deb6e07dd1fa5cf32c23e1901766859b6
Jack <jswim26@gmail.com>
2020-01-12 22:30:58 -0500

    [templates] base C++ header/source base

---
+++ templates/C++/Template.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Template.cpp
+ * @brief
+ * @author Jack Humphries (addr@gmail.com)
+ */
+
+#include "Template.hpp"
+ 
+// SYSTEM INCLUDES
+
+// PROJECT INCLUDES
+
+// LOCAL INCLUDES
+
+namespace C++ {
+
+
+} // namespace C++
---
+++ templates/C++/Template.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file Template.hpp
+ * @brief
+ * @author Jack Humphries (addr@gmail.com)
+ */
+
+#ifndef TEMPLATE_H_
+#define TEMPLATE_H_
+
+// SYSTEM INCLUDES
+
+// PROJECT INCLUDES
+
+// LOCAL INCLUDES
+
+namespace C++ {
+
+} // namespace C++
+
+#endif // TEMPLATE_H_
