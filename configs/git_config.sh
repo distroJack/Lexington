946ed14600f4cb50c2bd5b00c6296194073b518a
Jack Humphries <jswim26@gmail.com>
2020-03-22 14:03:25 -0400

    gitconfig remove global flags

--- configs/git_config.sh
+++ configs/git_config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git config --global user.name "Jack"
-git config --global user.email "jswim26@gmail.com"
+git config user.name "Jack"
+git config user.email "jswim26@gmail.com"
 
-git config --global alias.a "add"
-git config --global alias.b "branch"
-git config --global alias.c "commit"
-git config --global alias.p "push"
-git config --global alias.s "status"
+git config alias.a "add"
+git config alias.b "branch"
+git config alias.c "commit"
+git config alias.p "push"
+git config alias.s "status"
 
-git config --global core.editor "gedit"
+git config core.editor "gedit"
